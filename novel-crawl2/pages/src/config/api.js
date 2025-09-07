f694fe44aca7fb8acdd0c155a9cfc9cb752c975f
WebStory <webstory@live.jp>
2025-09-07 15:56:48 +0900

    正式服务器上websocket无法连接的问题对应

--- novel-crawl2/pages/src/config/api.js
+++ novel-crawl2/pages/src/config/api.js
@@ -6,10 +6,10 @@ const baselUrl = 'localhost,127.0.0.1'.indexOf(window.location.hostname) >= 0
 export const API_BASE_URL = `${baselUrl}`;
 
 // WebSocket 配置
-//const websocketUrl = window.location.protocol === 'https:' ? 'wss://novel.x.com/ws' : 'ws://127.0.0.1:30000/ws';
-const websocketUrl = 'localhost,127.0.0.1'.indexOf(window.location.hostname) >= 0
-    ? `ws://${window.location.hostname}:8584/ws`
-    : `wss://${window.location.host}/ws`;
+const websocketUrl = window.location.protocol === 'https:' ? 'wss://${window.location.host}/ws' : 'ws://${window.location.host}/ws';
+// const websocketUrl = 'localhost,127.0.0.1'.indexOf(window.location.hostname) >= 0
+//     ? `ws://${window.location.hostname}:8584/ws`
+//     : `wss://${window.location.host}/ws`;
 
 // console.log('websocketUrl:', websocketUrl);
 export const WS_URL = `${websocketUrl}`;
